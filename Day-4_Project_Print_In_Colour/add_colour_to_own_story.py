6e5014467d9c2ebe4ec36238bec14e494e916fc7
eknjustice <eknjustice@gmail.com>
2022-10-02 10:01:10 +0200

    print in colour

---
+++ Day-4_Project_Print_In_Colour/add_colour_to_own_story.py
@@ -0,0 +1,11 @@
+print("===Your adventure simulator===")
+print("""You'll be asked a bunch of questions then we'll make you up an amazing story with YOU as the star!""")
+print()
+name = input("Your name: ")
+enemy = input("Your worst enemy's name: ")
+superPower = input("Your super power: ")
+print()
+print('Our story begins as our hero', "\033[31m", name, "\033[0m", 'approaches a foreboding castle...')
+print("Suddenly a bolt of lightning striked the ground at the feet of", "\033[31m", name,".", "\033[0m")
+print("'Our final battle begins!' shouts the evil", "\033[33m", enemy, "\033[0m", "clearly missing the fact that", "\033[31m", name, "\033[0m", "has the power of", "\033[34m", superPower, "\033[0m" "which means they'll win quite easily")
+
